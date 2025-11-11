709f328fb96a2e137d65a2e0c0acfb9cbd7ddc19
Muhammed Enes <enessoftwareeng@gmail.com>
2025-11-11 19:51:57 +0300

    tasks tablosuna foreign key eklendi.

---
+++ database/migrations/2025_11_10_080155_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users');
+
+            $table->foreignId('category_id')
+                ->constrained('categories');
+
+            $table->string('title')->nullable();
+            $table->string('content');
+            $table->tinyInteger('status')->default(0)->comment('0-yapılmadı, 1-yapılıyor, 2-ertelendi, 3-iptal oldu');
+            $table->dateTime('deadline')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
